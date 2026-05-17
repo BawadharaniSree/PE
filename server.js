627159f224d348cd30a7435b4823731acc328213
BawadharaniSree <bawadharanisree@gmail.com>
2026-05-18 00:46:06 +0530

    for render 9

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "dist/client")));
 
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist/client/index.html"));
 });
 
